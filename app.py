381cc16472078ea5323a242dbffb858c584a5599
CjayAR <158511743+wes12345678910@users.noreply.github.com>
2026-05-08 11:49:59 -0400

    Update app.py 8909

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# On this specific Docker image, you don't need to provide a path
-driver = webdriver.Chrome(options=options)
+# This image has chrome installed at /usr/bin/google-chrome
+options.binary_location = "/usr/bin/google-chrome"
 
+driver = webdriver.Chrome(options=options)
